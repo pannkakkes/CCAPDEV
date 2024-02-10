a93bd4bfc2809631c228511bebd22082499d80da
pannkakkes <fiona.amaia@gmail.com>
2024-02-10 14:47:13 +0800

    Fazbear branding

--- js/User.js
+++ js/User.js
@@ -9,11 +9,11 @@ function Account(username, emailaddress, password, description, birthDate, profi
 }
 
 var users = [
-    new Account("FreddyFazbear", "freddyfazbear@dlsu.edu.ph" ,"fazbear00", "I am five bears.", "01/01/1987", "freddyfazbear.png", "V"),
-    new Account("BonnieBunny", "bonnythebonnybon@dlsu.edu.ph", "bonny3", "I like baking cupcakes.", "02/21/1983","bonniebunny.png" ,"V"),
-    new Account("FoxyThePirate", "foxythepirate@dlsu.edu.ph", "foxie12", "I hunt for treasure!", "06/04/1985", "foxythepirate.jpeg", "V"),
-    new Account("WilliamAfton", "williamaftersun@dlsu.edu.ph", "willom2", "I'm the bad guy.", "07/29/1954", "williamafton.jpg", "T"),
-    new Account("SpringTrap", "springtrap@dlsu.edu.ph", "Summer!", "I trap you.", "07/29/1954", "springtrap.jpg", "T")
+    new Account("Freddy Fazbear", "freddyfazbear@dlsu.edu.ph" ,"fazbear00", "I am five bears.", "01/01/1987", "freddyfazbear.png", "V"),
+    new Account("Bonnie Bunny", "bonnythebonnybon@dlsu.edu.ph", "bonny3", "I like baking cupcakes.", "02/21/1983","bonniebunny.png" ,"V"),
+    new Account("Foxy The Pirate", "foxythepirate@dlsu.edu.ph", "foxie12", "I hunt for treasure!", "06/04/1985", "foxythepirate.jpeg", "V"),
+    new Account("William Afton", "williamaftersun@dlsu.edu.ph", "willom2", "I'm the bad guy.", "07/29/1954", "williamafton.jpg", "T"),
+    new Account("Spring Trap", "springtrap@dlsu.edu.ph", "Summer!", "I trap you.", "07/29/1954", "springtrap.jpg", "T")
 ];
 
 function getUser(emailaddress){
