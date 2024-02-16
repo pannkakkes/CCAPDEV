af34e6eb34bf610c2311e11e9525bdcab61bc673
pannkakkes <fiona.amaia@gmail.com>
2024-02-16 11:52:42 +0800

    search now working

--- js/SearchSlots.js
+++ js/SearchSlots.js
@@ -1,4 +1,5 @@
-function validate() {
+function validate(event) {
+    event.preventDefault();
     var curDate = document.getElementById("selected_date").value;
     var curTime = document.getElementById("time_slot").value;
     var curLab = document.getElementById("selected_lab").value;
@@ -17,4 +18,23 @@ function validate() {
         alert("Please select a lab.");
         return false;
     }
+
+    if (curDate == "2024-01-01" && curTime === "slot12" && curLab === "lab1") {
+        document.getElementById("search-results").innerHTML = "Seats 4, 5, 6, 7, 8, 9, and 10 are available.";
+    }
+    else if (curDate == "2024-01-09" && curTime === "slot12" && curLab === "lab2") {
+        document.getElementById("search-results").innerHTML = "Seats 1, 2, 3, 4, 5, 6, 7, 8, and 10 are available.";
+    }
+    else if (curDate == "2024-01-08" && curTime === "slot12" && curLab === "lab3") {
+        document.getElementById("search-results").innerHTML = "Seats 1, 2, 3, 4, 5, 6, 7, 9, and 10 are available.";
+    }
+    else if (curDate == "2024-01-07" && curTime === "slot12" && curLab === "lab3") {
+        document.getElementById("search-results").innerHTML = "Seats 1, 2, 3, 4, 5, 6, 8, 9, and 10 are available.";
+    }
+    else {
+        document.getElementById("search-results").innerHTML = "All seats are available!";
+    }
+
+    document.getElementById("search-results").style.display = "block";
+    return false;
 }
