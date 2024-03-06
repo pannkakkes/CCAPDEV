22f79198d6ebb07bf0e6f0567e00e4b089dd6c48
pannkakkes <fiona.amaia@gmail.com>
2024-03-06 22:30:55 +0800

    moved sample data

--- database/models/Reservation.js
+++ database/models/Reservation.js
@@ -12,19 +12,4 @@ const ReservationSchema = new mongoose.Schema({
 
 const Reservation = mongoose.model('Reservation', ReservationSchema)
 
-Reservation.create([
-    { reserveId: 1001, username: 'FreddyFazbear', seat: 'Seat 1', laboratory: 'Freddy\'s Frightful Manor', dateTimeRequest: '1/1/2024 10:00 AM',
-    dateTimeReservation: '1/1/2024 2:30 PM', isAnonymous: false},
-    { reserveId: 1002, username: 'FreddyFazbear', seat: 'Seat 9', laboratory: 'Chica\'s Chilling Chamber', dateTimeRequest: '1/1/2024 10:01 AM',
-    dateTimeReservation: '1/9/2024 2:30 PM', isAnonymous: false},
-    { reserveId: 1003, username: 'FreddyFazbear', seat: 'Seat 8', laboratory: 'Puppet\'s Perilous Palace', dateTimeRequest: '1/1/2024 10:02 AM',
-    dateTimeReservation: '1/8/2024 2:30 PM', isAnonymous: false},
-    { reserveId: 1004, username: 'FreddyFazbear', seat: 'Seat 7', laboratory: 'Puppet\'s Perilous Palace', dateTimeRequest: '1/1/2024 10:03 AM',
-    dateTimeReservation: '1/7/2024 2:30 PM', isAnonymous: false},
-    { reserveId: 1005, username: 'BonnieBunny', seat: 'Seat 2', laboratory: 'Freddy\'s Frightful Manor', dateTimeRequest: '1/1/2024 10:00 AM',
-    dateTimeReservation: '1/1/2024 2:30 PM', isAnonymous: false},
-    { reserveId: 1006, username: 'FoxyThePirate', seat: 'Seat 3', laboratory: 'Freddy\'s Frightful Manor', dateTimeRequest: '1/1/2024 10:00 AM',
-    dateTimeReservation: '1/1/2024 2:30 PM', isAnonymous: true}
-])
-
 module.exports = Reservation
--- index.js
+++ index.js
@@ -18,6 +18,33 @@ app.use(fileUpload())
 var hbs = require('hbs')
 app.set('view engine','hbs');
 
+User.create([
+    { username: 'FreddyFazbear', email: 'freddyfazbear@dlsu.edu.ph', password: 'fazbear00', description: 'I am five bears.',
+    birthdate: '01/01/1987', role: 'V' },
+    { username: 'BonnieBunny', email: 'bonnythebonnybon@dlsu.edu.ph', password: 'bonny3', description: 'I like baking cupcakes.',
+    birthdate: '02/21/1983', role: 'V' },
+    { username: 'FoxyThePirate', email: 'foxythepirate@dlsu.edu.ph', password: 'foxie12', description: 'I hunt for treasure!',
+    birthdate: '06/04/1985', role: 'V' },
+    { username: 'WilliamAfton', email: 'williamaftersun@dlsu.edu.ph', password: 'willom2', description: "I'm the bad guy.",
+    birthdate: '07/29/1954', role: 'T' },
+    { username: 'SpringTrap', email: 'springtrap@dlsu.edu.ph', password: 'Summer!', description: "I trap you.",
+    birthdate: '07/29/1954', role: 'T' },
+]);
+
+Reservation.create([
+    { reserveId: 1001, username: 'FreddyFazbear', seat: 'Seat 1', laboratory: 'Freddy\'s Frightful Manor', dateTimeRequest: '1/1/2024 10:00 AM',
+    dateTimeReservation: '1/1/2024 2:30 PM', isAnonymous: false},
+    { reserveId: 1002, username: 'FreddyFazbear', seat: 'Seat 9', laboratory: 'Chica\'s Chilling Chamber', dateTimeRequest: '1/1/2024 10:01 AM',
+    dateTimeReservation: '1/9/2024 2:30 PM', isAnonymous: false},
+    { reserveId: 1003, username: 'FreddyFazbear', seat: 'Seat 8', laboratory: 'Puppet\'s Perilous Palace', dateTimeRequest: '1/1/2024 10:02 AM',
+    dateTimeReservation: '1/8/2024 2:30 PM', isAnonymous: false},
+    { reserveId: 1004, username: 'FreddyFazbear', seat: 'Seat 7', laboratory: 'Puppet\'s Perilous Palace', dateTimeRequest: '1/1/2024 10:03 AM',
+    dateTimeReservation: '1/7/2024 2:30 PM', isAnonymous: false},
+    { reserveId: 1005, username: 'BonnieBunny', seat: 'Seat 2', laboratory: 'Freddy\'s Frightful Manor', dateTimeRequest: '1/1/2024 10:00 AM',
+    dateTimeReservation: '1/1/2024 2:30 PM', isAnonymous: false},
+    { reserveId: 1006, username: 'FoxyThePirate', seat: 'Seat 3', laboratory: 'Freddy\'s Frightful Manor', dateTimeRequest: '1/1/2024 10:00 AM',
+    dateTimeReservation: '1/1/2024 2:30 PM', isAnonymous: true}
+])
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '\\' + 'index.html');
