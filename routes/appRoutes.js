e683ff9c49d0d6bfc1aa0a400299a0bcc120bfdc
JoaquinRodriguezph <109897751+JoaquinRodriguezph@users.noreply.github.com>
2024-04-06 11:22:54 +0800

    attempt n

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -93,7 +93,11 @@ router.get('/app/userregister', function (req, res) {
 });
 
 router.post('/register', async function (req, res) {
-    const { image } = req.files;
+    if (req.files){
+        const { image } = req.files;
+    }
+    else{
+    }
     const { email, username, password, description, birthdate, role } = req.body;
 
     try {
@@ -116,11 +120,15 @@ router.post('/register', async function (req, res) {
 
             try {
                 // Generate unique filename with date and time
-                const timestamp = moment().format('YYYYMMDDHHmmss');
-                const fileExtension = path.extname(image.name);
-                const uniqueFilename = `${timestamp}_${username}${fileExtension}`;
-
-                await image.mv(path.resolve(__dirname, '../public/images', uniqueFilename));
+                if (req.files){
+                    const timestamp = moment().format('YYYYMMDDHHmmss');
+                    const fileExtension = path.extname(image.name);
+                    const uniqueFilename = `${timestamp}_${username}${fileExtension}`;
+                    await image.mv(path.resolve(__dirname, '../public/images', uniqueFilename));
+                }
+                else{
+                    const uniqueFilename = 'default.jpg';
+                }
 
                 // Transform birthdate to mm/dd/yyyy format
                 const formattedBirthdate = moment(birthdate, 'YYYY-MM-DD').format('MM/DD/YYYY');
