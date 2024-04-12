b3fcc8a3f70e61e115a43338ee891299496a53b6
pannkakkes <fiona.amaia@gmail.com>
2024-04-12 22:05:32 +0800

    Update mainRoutes.js

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -4,7 +4,7 @@ const reserve = require("./reserveRoutes");
 const search = require("./searchRoutes");
 
 router.get('/', function (req, res) {
-    res.render("dashboard", {layout: "layouts/main"});
+    return res.render("dashboard", {layout: "layouts/main"});
 });
 
 router.use("/profile", profile);
@@ -15,13 +15,13 @@ router.use("/search", search);
 
 router.get('/userlogout', function (req, res) {
     const currentUser = req.session.currentUser;
-    res.render('userlogout', { currentUser, layout:"layouts/main"});
+    return res.render('userlogout', { currentUser, layout:"layouts/main"});
 });
 
 router.get('/logout', function (req, res) {
     req.session.currentUser = null; // Reset currentUser in session
     res.sendStatus(200); // Send a success response
-    res.render('index', { layout:"layouts/main"});
+    return res.render('index', { layout:"layouts/main"});
 });
 
 module.exports = router;
